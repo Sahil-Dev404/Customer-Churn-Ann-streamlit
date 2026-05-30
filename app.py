61876b484690e0fe30a6671c8f5620c99ebf796d
Sahil-Dev404 <2005sahilsaini@gmail.com>
2026-05-30 20:02:46 +0530

    chnages

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open('scaler.pkl', 'rb') as file:
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Churn Prediction')
 
 # User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
@@ -63,8 +63,9 @@ prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
 st.write(f'Churn Probability: {prediction_proba:.2f}')
+st.write("Raw prediction:", prediction_proba)
 
-if prediction_proba > 0.5:
+if prediction_proba > 0.4:
     st.write('The customer is likely to churn.')
 else:
     st.write('The customer is not likely to churn.')
